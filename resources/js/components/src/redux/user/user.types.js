5ce049f3029c7ea51029cd785ee1a89dd2f1adcd
yacouzz <44506890+yacouzz@users.noreply.github.com>
2020-01-02 14:48:09 +0100

    Update user.types.js

--- resources/js/components/src/redux/user/user.types.js
+++ resources/js/components/src/redux/user/user.types.js
@@ -1,5 +1,6 @@
 const userTypesActions ={
-    SET_CURRENT_USER:'SET_CURRENT_USER'
+    SET_CURRENT_USER:'SET_CURRENT_USER',
+    DELETE_CURRENT_USER:'DELETE_CURRENT_USER'
 }
 
 export default userTypesActions;
