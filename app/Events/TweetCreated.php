2fd32cc35648e6db553e77cb57549e4f0ba19cb1
Rabah Hammoutene <44506890+yacouzz@users.noreply.github.com>
2020-02-05 16:10:23 +0100

    Update For Real-time 2

--- app/Events/TweetCreated.php
+++ app/Events/TweetCreated.php
@@ -33,6 +33,19 @@ class TweetCreated implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        return new Channel('new-post');
+        return [
+            new Channel('new-post'),
+            new Channel('App.User.'.$this->post->user->id)
+        ];
+    }
+
+    public function broadcastWith(){
+        return [
+            'post'=> array_merge($this->post->toArray(), [
+                'user'=> $this->post->user
+            ]
+            ),
+            'user'=> $this->user,
+        ];
     }
 }
